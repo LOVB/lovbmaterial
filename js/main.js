0640ea533029319d334e80999347d856e4d99ad8
lovb <lovbstudio@gmail.com>
2019-07-27 21:44:56 +0200

    imagenes, enlaces y javascrip

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* Iniciar todos los eventos de JavaScript en el proyecto */
+document.addEventListener('DOMContentLoaded', function(){
+  M.AutoInit();
+});
+
+/* Iniciación de evento de MENU en SIDE BAR */
+/* document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+}); */
