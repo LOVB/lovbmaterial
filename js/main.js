25816f34b33c14420ee29c10e0c87f64f7449704
lovb <lovbstudio@gmail.com>
2019-08-22 17:46:49 +0200

    Rectificaciones en el carousel de marketing

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 document.addEventListener('DOMContentLoaded', function(){
   M.AutoInit();
 });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+   var instance = M.Carousel.init(elems, {
+      fullWidth: true,
+      indicators: true
+  });
+});
